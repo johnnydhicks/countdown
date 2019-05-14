692b7d0466bb63f6d8faf95873ccecd7848e89bf
Johnny Hicks <johnnydhicks@gmail.com>
2019-05-13 19:54:19 -0600

    First commit

---
+++ CountDown (iOSPT1)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountDown (iOSPT1)
+//
+//  Created by Johnny Hicks on 5/13/19.
+//  Copyright © 2019 Johnny Hicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
